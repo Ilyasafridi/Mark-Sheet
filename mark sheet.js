e52413e1589b453ddea3799c3996a94d8f3a2044
Ilyasafridi <abidullahafridi936@gmail.com>
2024-01-16 08:17:00 -0800

    Mark-Sheet

---
+++ mark sheet.js
@@ -0,0 +1,29 @@
+let mark = prompt('please inter your mark and chick the inspect')
+
+if ( mark > 90 ) {
+    console.log('You Are A+ Grade')
+}
+
+else if ( mark > 80 ) {
+    console.log('You Are A Grade')
+}
+
+else if ( mark > 70 ) {
+    console.log('You Are B Grade')
+}
+
+else if ( mark > 60 ) {
+    console.log('You Are C Grade')
+}
+
+else if ( mark > 50 ) {
+    console.log('You Are D Grade')
+}
+
+else if ( mark > 40 ) {
+    console.log('You Are E Grade Week')
+}
+
+else {
+    console.log('You Are Fail')
+}
